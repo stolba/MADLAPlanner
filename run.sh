97846dbfc1812ff68d6ae6a02c35b3da818392e3
michal stolba <stolba@stolba-PC.(none)>
2016-02-05 15:19:47 +0100

    cleaning

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-./parallel_experiments.sh -j solver.jar -c instances/$1/data.in -o instances/$1/data.out -v -s $2/:  
-./addhead.sh instances/$1/data.out
--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mvn clean
-mvn package
-cp target/madla-planner-1.0-SNAPSHOT-jar-with-dependencies.jar release/madla-planner.jar
-cp -rf benchmarks/ release/
-cp -rf ma-benchmarks/ release/
-cp -rf misc/ release/
